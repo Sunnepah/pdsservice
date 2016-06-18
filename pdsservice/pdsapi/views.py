51bf027eee587747a597895e92321655387ab141
Sunday Ayandokun <sunday.ayandokun@gmail.com>
2016-06-18 23:29:24 +0300

    Added query to fetch address graph data

--- pdsservice/pdsapi/views.py
+++ pdsservice/pdsapi/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from SPARQLWrapper.Wrapper import SPARQLWrapper, JSON
+from sparqlwrapper.SPARQLWrapper import SPARQLWrapper, JSON, XML, N3, RDF, TURTLE
 
 SPARQL_ENDPOINT = settings.SPARQL_ENDPOINT
 SPARQL_AUTH_ENDPOINT = settings.SPARQL_AUTH_ENDPOINT
@@ -22,8 +22,16 @@ def index(request):
 def get_user_email(request, user_id):
     try:
         email = query_graph(get_email_graph_uri(user_id))
+        value = email[0]["email"]["value"].split(":")[1]
 
-        return Response({'email': email}, status=status.HTTP_200_OK)
+        telephone = query_graph_telephone(get_telephone_graph_uri(user_id))
+        phone = telephone[0]["phone"]["value"].split(":")[1]
+
+        address = query_graph_address(get_address_graph_uri(user_id))
+        full_address = address[0]["street"]["value"] + "," + address[0]["locality"]["value"] + "," + \
+                       address[0]["postal_code"]["value"] + "," + address[0]["country_name"]["value"]
+
+        return Response({"email": value, "telephone":phone, "address":full_address}, status=status.HTTP_200_OK)
 
     except Exception as e:
         print e.message
@@ -46,7 +54,7 @@ def query_graph(graph):
         sparql = SPARQLWrapper(SPARQL_AUTH_ENDPOINT)
         sparql.setCredentials(settings.VIRTUOSO_USER, settings.VIRTUOSO_PASSW)
 
-        query = "SELECT * WHERE { GRAPH <" + graph + "> { ?s ?p ?o . } }"
+        query = "SELECT ?email WHERE { GRAPH <" + graph + "> { ?s <http://www.w3.org/2006/vcard/ns#hasEmail> ?o . ?o <http://www.w3.org/2006/vcard/ns#hasValue> ?email} }"
         print query
         sparql.setQuery(query)
 
@@ -62,6 +70,53 @@ def query_graph(graph):
         print e.message
         return list()
 
+def query_graph_telephone(graph):
+    try:
+        sparql = SPARQLWrapper(SPARQL_AUTH_ENDPOINT)
+        sparql.setCredentials(settings.VIRTUOSO_USER, settings.VIRTUOSO_PASSW)
+
+        query = "SELECT ?phone WHERE { GRAPH <" + graph + "> { ?s <http://www.w3.org/2006/vcard/ns#hasTelephone> ?o . ?o <http://www.w3.org/2006/vcard/ns#hasValue> ?phone} }"
+        print query
+        sparql.setQuery(query)
+
+        # JSON example
+        # print '\n\n*** JSON Example'
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+
+        res = json.dumps(results, separators=(',', ':'))
+        print res
+        return results['results']['bindings']
+    except Exception as e:
+        print e.message
+        return list()
+
+def query_graph_address(graph):
+    try:
+        sparql = SPARQLWrapper(SPARQL_AUTH_ENDPOINT)
+        sparql.setCredentials(settings.VIRTUOSO_USER, settings.VIRTUOSO_PASSW)
+
+        query = "SELECT ?street,?locality,  ?postal_code, ?country_name" \
+                " WHERE { GRAPH <" + graph + "> { ?s <http://www.w3.org/2006/vcard/ns#hasAddress> ?add . " \
+                "?add <http://www.w3.org/2006/vcard/ns#street-address> ?street . " \
+                "?add <http://www.w3.org/2006/vcard/ns#locality> ?locality . " \
+                "?add <http://www.w3.org/2006/vcard/ns#postal-code> ?postal_code . " \
+                "?add <http://www.w3.org/2006/vcard/ns#country-name> ?country_name } }"
+
+        print query
+        sparql.setQuery(query)
+
+        # JSON example
+        # print '\n\n*** JSON Example'
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+
+        res = json.dumps(results, separators=(',', ':'))
+        print res
+        return results['results']['bindings']
+    except Exception as e:
+        print e.message
+        return list()
 
 def get_email_graph_uri(user_id):
     print settings.GRAPH_ROOT + '/' + user_id + '/emails'
